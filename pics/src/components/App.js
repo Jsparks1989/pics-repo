6765584a7ebaac89778eb2623fbb34e6c5c83217
Jsparks1989 <jsparks19891@gmail.com>
2019-12-09 22:17:37 -0500

    async/await version of previous commit.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,19 +5,19 @@ import SearchBar from './SearchBar';
 // Class component
 class App extends React.Component {
 
-    onSearchSubmit  = (term) => {
+    onSearchSubmit  = async (term) => {
         // Want to make API request to unsplash when this function is invoked. 
         // Must use GET request with an endpoint of /search/photos.
         // Takes 2 params. First: the address to make GET request to. 
         // Two: object that can customize request.
-        axios.get('https://api.unsplash.com/search/photos', {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 0728ec1aafa1cad5807f767267288d5ece7b6b4cb11454283bc5a2dc983785b4'
             }
-        }).then((response)=>{ console.log(response.data.results); });
+        });
 
-        console.log();
+        console.log(response.data.results);
     }
 
     render(){
