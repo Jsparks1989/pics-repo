4c9e107f4c8dd52279fbe1ec72ba2eadc5336d86
Jsparks1989 <jsparks19891@gmail.com>
2019-12-10 20:04:00 -0500

    For some reason, making props.images.map in a function does not work. Only works when setting to a variable.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const ImageList = (props) => {
 
     const list = props.images.map((image) => {
@@ -8,6 +7,16 @@ const ImageList = (props) => {
             <img src={image.urls.regular}></img>
             );
         });
+
+    //-- For some reason, this syntax does not work. --//
+    // const list = () => {
+    //     props.images.map((image) => {
+    //         return(
+    //             <img src={image.urls.regular}></img>
+    //         );
+    //     });
+    // }
+
 
     return(
         <div>
