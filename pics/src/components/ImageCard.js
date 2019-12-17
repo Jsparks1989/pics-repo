dd92eba1fc5cd41704898903dac43e0157143f61
Jsparks1989 <jsparks19891@gmail.com>
2019-12-17 18:01:15 -0500

    Cant access image height from componentDidMount(), images havent loaded yet in order to get the height. Find a work around.

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    /*
+     * Creating a Ref in the constructor. Ref gives us access to a single DOM element.
+     * Using Ref to get the height and width of each individual image, so we can customize
+     * each images space to show in CSS.
+     */
+    constructor(props){
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef.current);
+
+        /*
+         * Console.logging 0 for clientHeight because we are console logging  
+         * the height before the image has even had a change to load. So the image 
+         * tag has a height of 0 pixels. Getting the height of image from the componentDidMount()
+         * is way too early. Must get image height from somewhere else.
+         */
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
         // Destructuring image object.
         const {description, urls} = this.props.image;
@@ -8,6 +30,7 @@ class ImageCard extends React.Component {
         return(
             <div>
                 <img 
+                ref={this.imageRef}
                 alt={description}
                 src={urls.regular}>
                 </img>
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,7 +3,7 @@ import './imageList.css';
 import ImageCard from './ImageCard';
 
 
-/**
+/*
  * Functional Components are just functions, so they have no 'this'.  
  * To understand React components, you'll need to understand JS classes first.
  * The 'this' keyword, essentially, is a reference to an object that is assigned
@@ -13,14 +13,10 @@ import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 
-    // console.log('ImageList props: ');
-    // console.log(props.images);
-
     // BEST PRACTICE FOR MAPPING OVER LISTS
     // ====================================
     const list = props.images.map((image) => {
             return(
-            // <img key={image.id} src={image.urls.regular}></img>
             <ImageCard key={image.id} image={image}/>
             );
         });
