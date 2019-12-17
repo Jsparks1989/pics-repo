552ecebea981507a0770db41689cc615b6051cb9
Jsparks1989 <jsparks19891@gmail.com>
2019-12-17 18:30:05 -0500

    set the height of each image in setSpans() and calculated the amount of spans needed for each image and set that to state.spans

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,38 +1,37 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
-    /*
-     * Creating a Ref in the constructor. Ref gives us access to a single DOM element.
-     * Using Ref to get the height and width of each individual image, so we can customize
-     * each images space to show in CSS.
-     */
     constructor(props){
         super(props);
+
+        this.state = { spans: 0 };
+
         this.imageRef = React.createRef();
     }
 
+    // Runs when ImageCard component mounts.
     componentDidMount(){
-
-        // Listening for the event 'load'. When images have successfully loads up
-        // or downloads from the internet and is displayed obn screen, it emits the 'load'
-        // event. Once load event has been emitted, the images have been successfully loaded 
-        // and we can get the actual height of the image. Calling callback function setSpans().
+        // When each individual image has loaded, run the callback function setSpan.
         this.imageRef.current.addEventListener('load', this.setSpans);
     }
 
-
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        // console.log(this.imageRef.current.clientHeight);
+
+        const height = this.imageRef.current.clientHeight;
+
+        // Calculate how many spans to set to grid-row-end in CSS.
+        const spans = Math.ceil(height / 150);
+
+        this.setState({ spans });
     }
-
-
 
     render() {
         // Destructuring image object.
         const {description, urls} = this.props.image;
 
         return(
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img 
                 ref={this.imageRef}
                 alt={description}
