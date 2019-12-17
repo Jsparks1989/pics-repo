b632e9d0033755f8eaab1638ccc9e546ee3a75c1
Jsparks1989 <jsparks19891@gmail.com>
2019-12-17 16:37:48 -0500

    Brief explanation as to why functional components do not have 'this' and class components do.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './imageList.css';
 import ImageCard from './ImageCard';
 
+
+/**
+ * Functional Components are just functions, so they have no 'this'.  
+ * To understand React components, you'll need to understand JS classes first.
+ * The 'this' keyword, essentially, is a reference to an object that is assigned
+ *  based on how the function is called (execution context).
+ */
+
+
 const ImageList = (props) => {
 
     // console.log('ImageList props: ');
